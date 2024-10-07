768bab72c1cdfe0e5ff3ea3d04768063935c9807
Fadliabdill <fadliabdill287@gmail.com>
2024-10-07 12:55:26 +0700

    halaman karyawan

--- resources/views/karyawan/page/modal.blade.php
+++ resources/views/karyawan/page/modal.blade.php
@@ -12,11 +12,26 @@
                         <div class="form-group row">
                             <label for="inputPassword3" class="col-sm-2 col-form-label">Kategori</label>
                             <div class="col-sm-10">
-                                <select class="form-control select2bs4 select2-hidden-accessible" name="role" id="">
+                                <select class="form-control select2bs4 select2-hidden-accessible" name="role"
+                                    id="">
                                     @foreach ($kategori as $item)
-                                    <option value="{{$item->kode}}">{{$item->name}}</option>
+                                        <option value="{{ $item->kode }}">{{ $item->name }}</option>
                                     @endforeach
                                 </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Foto Produk</label>
+                            <div class="col-sm-10">
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="form-group row">
